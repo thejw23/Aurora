30c89b5d99d6669fcaadac58f7aba540d1a3fc8c
José Miguel Molina Arboledas <rd4091@gmail.com>
2013-10-20 16:52:12 +0200

    Tabs -> spaces @ \Aurora\Type

--- Aurora/Type.php
+++ Aurora/Type.php
@@ -4,14 +4,14 @@ namespace Aurora;
 
 abstract class Type
 {
-	abstract public function isValidValue($value);
-	abstract public function getRepresentation();
+    abstract public function getRepresentation();
+    abstract public function isValidValue($value);
     abstract public function parseValue($value);
 	
-	final public function getDriver() {
-		$driver = \Aurora\Dbal::getDriver();
-		if (is_null($driver))
-			throw new DatabaseException('Database driver must be configured before the creation of any \Aurora\Type instance.');
-		return $driver;
-	}
+    final public function getDriver() {
+        $driver = \Aurora\Dbal::getDriver();
+        if (is_null($driver))
+            throw new DatabaseException('Database driver must be configured before the creation of any \Aurora\Type instance.');
+        return $driver;
+    }
 }
